55e269823ed243ea02ecdfb5b425b2575c9aec0c
林佑宥 <92373659+user-4314@users.noreply.github.com>
2023-05-06 12:21:25 +0800

    add DivideDifference and  DifferenceTable

--- Interpolation/Interpolation.cpp
+++ Interpolation/Interpolation.cpp
@@ -2,15 +2,16 @@
 #include <cstdio>
 using namespace std;
 const int N = 3;
-double X[N] = {-2,0,1},Y[N] = {9,-1,-3};//線上的點
+double X[N] = {-2,0,2},Y[N] = {9,-1,-3};//線上的點
 double KnownCoefficient[N] = {1,-3,-1};//方程式的係數
+double DDT[N][N] = {0},FDT[N][N] = {0};
 
 void Interpolation(double x){
     double P = KnownCoefficient[0];
     for(int i=1;i<N;i++)
         P = P * x + KnownCoefficient[i];
 
-    printf("bring %.2f in f(x) by Interpolation y is %f\n",x,P);
+    printf("bring x = %.2f by Interpolation then y = %f\n",x,P);
 }
 
 void LagramgeMethod(double x){
@@ -25,15 +26,65 @@ void LagramgeMethod(double x){
         }
         sum = sum + P*Y[k];
     }
-    printf("bring %.2f in f(x) by Lagramge y is %f\n",x,sum);
+    printf("bring x = %.2f by Lagramge then y = %f\n",x,sum);
+}
+
+void DivideDifference(double x){
+    int dis = 1;
+    for(int i=0;i<N;i++)
+        DDT[0][i] = Y[i];
+    for(int i=1;i<N;i++){
+        for(int j=0;j<N-i;j++)
+            DDT[i][j]= (DDT[i-1][j+1]-DDT[i-1][j]) / (X[j+dis]-X[j]);
+        dis++;
+    }
+
+    double y = DDT[0][0];
+    for(int i=1;i<N;i++){
+        double P = 1;
+        for(int j=0;j<i;j++)
+            P = P * (x-X[j]);
+        y = y + P*DDT[i][0];
+    }
+    printf("bring x = %.2f by Divide Difference y = %f\n",x,y);
+}
+
+
+void DifferenceTable(double x){
+    for(int i=0;i<N;i++)
+        FDT[0][i] = Y[i];
+    for(int i=1;i<N;i++){
+        for(int j=0;j<N-i;j++)
+            FDT[i][j]= FDT[i-1][j+1]-FDT[i-1][j];
+    }
+
+    double s = (x-X[0]) / abs( (X[1]-X[0]) );
+    double y = FDT[0][0];
+    for(int i=1;i<N;i++){
+        double P = 1;
+        for(int j=0;j<i;j++)
+            P = P * (s-j) / (j+1);
+        y = y + P*FDT[i][0];
+    }
+    printf("bring x = %.2f by Front Difference Table then y = %f\n",x,y);
 }
 
 int main()
 {
     double x = -2.7;
     Interpolation(x);
+    printf("\n");
 
     for(double i=-3;i<3;i+=0.1)
         LagramgeMethod(i);
+    printf("\n");
+
+    x = 2.4;
+    DivideDifference(x);
+    printf("\n");
+
+     x = -0.8;
+    DifferenceTable(x);
+    printf("\n");
     return 0;
 }
